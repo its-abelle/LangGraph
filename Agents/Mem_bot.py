901ec29fc3cfddaa2cee324857d77c8fbca09c1f
its-abelle <aquinarian@gmail.com>
2026-08-07 09:32:41 +0300

    Chatbot-ish agent with mem

---
+++ Agents/Mem_bot.py
@@ -0,0 +1,37 @@
+import os
+from typing import TypedDict, List, Union
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_qwq import ChatQwen
+from langgraph.graph import StateGraph, START, END
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class AgentState(TypedDict):
+    messages: List[Union[HumanMessage, AIMessage]]
+
+llm = ChatQwen(model="qwen3.8-max")
+
+def process(state: AgentState) -> AgentState:
+    """This node will solve the input returned"""
+    response = llm.invoke(state["messages"])
+    print(f"\nAI: {response.content}")
+    state["messages"].append(response)
+    return state
+
+graph = StateGraph(AgentState)
+graph.add_node("process", process)
+graph.add_edge(START, "process")
+graph.add_edge("process", END)
+agent = graph.compile()
+
+conversation_history = []
+
+user_input = input("Enter: ")
+while user_input.lower() != "exit":
+    conversation_history.append(HumanMessage(content=user_input))
+    result = agent.invoke({"messages": conversation_history})
+
+    user_input = input("Enter: ")
+
