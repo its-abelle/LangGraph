d8bdda039f94eca8f49996e7ed754050b1a9b264
its-abelle <aquinarian@gmail.com>
2026-08-07 05:21:46 +0300

    chatbot-ish without memory

--- Agents/bot_agent.py
+++ Agents/bot_agent.py
@@ -24,7 +24,11 @@ graph.add_edge("process", END)
 agent = graph.compile()
 
 user_input = input("Enter: ")
-agent.invoke({"messages": [HumanMessage(content=user_input)]})
+while user_input.lower() != "exit":
+    agent.invoke({"messages": [HumanMessage(content=user_input)]})
+    user_input = input("Enter: ")
+
+
 
 
 
